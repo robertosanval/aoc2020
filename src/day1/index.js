0bdcd36ce54e4121035a39b57f5fe39df1e2cf4e
Roberto Santana <roberto@alpex-digital.com>
2020-12-01 11:15:33 +0100

    Day 1 solution

---
+++ src/day1/index.js
@@ -0,0 +1,53 @@
+const { test, readInput } = require("../utils")
+
+const prepareInput = (rawInput) => rawInput
+
+const input = prepareInput(readInput())
+
+const goA = (input) => {
+  let result = 0;
+  const numbers = input
+    .split('\n')
+    .map(number => parseInt(number));
+
+  numbers.forEach(n1 => {
+    numbers.forEach(n2 => {
+      if (n1 + n2 === 2020) {
+        result = n1 * n2;
+      }
+    });
+  });
+
+  return result;
+}
+
+const goB = (input) => {
+  let result = 0;
+  const numbers = input
+    .split('\n')
+    .map(number => parseInt(number));
+
+  numbers.forEach(n1 => {
+    numbers.forEach(n2 => {
+      numbers.forEach(n3 => {
+        if (n1 + n2 + n3 === 2020) {
+          result = n1 * n2 * n3;
+        }
+      })
+    });
+  });
+
+  return result;
+}
+
+/* Tests */
+// test(goA(input), 514579)
+
+/* Results */
+console.time("Time")
+const resultA = goA(input)
+const resultB = goB(input)
+console.timeEnd("Time")
+
+console.log("Solution to part 1:", resultA)
+console.log("Solution to part 2:", resultB)
