6d972e525c3ba8f99edadb6e32702ca96b1a8d0f
Roberto Santana <roberto@alpex-digital.com>
2020-12-02 08:50:52 +0100

    Day 2 solution

---
+++ src/day2/index.js
@@ -0,0 +1,76 @@
+const { parse } = require("path");
+const { test, readInput } = require("../utils")
+
+const prepareInput = (rawInput) => rawInput
+
+const input = prepareInput(readInput())
+
+const goA = (input) => {
+  const regex = /(\d+)-(\d+) ([a-z]): ([a-z]+)/gm;
+  const count = (char, word) => {
+    const re = new RegExp(char, "ig");
+    return ((word || '').match(re) || []).length;
+  }
+
+  let validPasswords = 0;
+  while ((m = regex.exec(input)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+
+    const min = parseInt(m[1]);
+    const max = parseInt(m[2]);
+    const letter = m[3];
+    const password = m[4];
+    const occurrences = count(letter, password);
+
+    if (occurrences >= min && occurrences <= max) {
+      validPasswords++;
+    }
+  }
+
+  return validPasswords;
+}
+
+const goB = (input) => {
+const regex = /(\d+)-(\d+) ([a-z]): ([a-z]+)/gm;
+  const count = (char, word) => {
+    const re = new RegExp(char, "ig");
+    return ((word || '').match(re) || []).length;
+  }
+
+  let validPasswords = 0;
+  while ((m = regex.exec(input)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+
+    const min = parseInt(m[1]);
+    const max = parseInt(m[2]);
+    const letter = m[3];
+    const password = m[4];
+    const matchFirst = password.substr(min - 1, 1) === letter;
+    const matchSecond = password.substr(max - 1, 1) === letter;
+
+    if (matchFirst ^ matchSecond) {
+      validPasswords++;
+    }
+
+  }
+
+  return validPasswords;
+}
+
+/* Tests */
+// test(goB(input), 428)
+
+/* Results */
+console.time("Time")
+const resultA = goA(input)
+const resultB = goB(input)
+console.timeEnd("Time")
+
+console.log("Solution to part 1:", resultA)
+console.log("Solution to part 2:", resultB)
