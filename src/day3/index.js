3296a017062c501b4727ead4062e26ccc0a65d28
Roberto Santana <roberto@alpex-digital.com>
2020-12-03 08:12:19 +0100

    Day 3. Solution 2

--- src/day3/index.js
+++ src/day3/index.js
@@ -4,19 +4,15 @@ const prepareInput = (rawInput) => rawInput;
 
 const input = prepareInput(readInput());
 
-const goA = (input) => {
+const goA = (input, right, down) => {
   const area = input.trim().split('\n');
-  const right = 3;
-  const down = 1;
   const tree = '#';
-
   const width = area[0].length;
   const height = area.length;
-  let posX = 3;
-  let posY = 1;
-  let trees = 0;
 
-  console.log({ width, height });
+  let posX = right;
+  let posY = down;
+  let trees = 0;
 
   while (posY < height) {
     if (area[posY][posX % width] === tree) {
@@ -31,15 +27,20 @@ const goA = (input) => {
 }
 
 const goB = (input) => {
-  return
+  return goA(input, 1, 1) *
+         goA(input, 3, 1) *
+         goA(input, 5, 1) *
+         goA(input, 7, 1) *
+         goA(input, 1, 2);
 }
 
 /* Tests */
-test(goA(input), 7);
+// test(goA(input, 3, 1), 7);
+// test(goB(input), 336);
 
 /* Results */
 console.time("Time 1");
-const resultA = goA(input);
+const resultA = goA(input, 3, 1);
 console.timeEnd("Time 1");
 
 console.time("Time 2");
