21052c1e51b230acbf76fb51a1067d7159d900a9
Roberto Santana <roberto@alpex-digital.com>
2020-12-17 20:09:02 +0100

    😰😰😰

---
+++ src/day17/index.js
@@ -0,0 +1,29 @@
+const { test, readInput } = require("../utils");
+
+const prepareInput = (rawInput) => rawInput;
+
+const input = prepareInput(readInput());
+
+const goA = (input) => {
+  return
+}
+
+const goB = (input) => {
+  return
+}
+
+/* Tests */
+// test(goA(input), expected);
+// test(goB(input), expected);
+
+/* Results */
+console.time("Time 1");
+const resultA = goA(input);
+console.timeEnd("Time 1");
+
+console.time("Time 2");
+const resultB = goB(input);
+console.timeEnd("Time 2");
+
+console.log("Solution to part 1: ", resultA);
+console.log("Solution to part 2: ", resultB);
